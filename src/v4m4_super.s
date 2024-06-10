8691ecf13cafeeef68362ade87bb4100684d9d0b
XProger <xproger@list.ru>
2024-06-10 03:37:53 +0300

    optimize v4m44 (24.8 ms)

--- src/v4m4_super.s
+++ src/v4m4_super.s
@@ -10,6 +10,7 @@ CENTER_Y        EQU ((240/2) << 13)
 
 MADAM           EQU 0x03300000
 MATH_STACK      EQU 0x0600
+MATH_STATUS     EQU 0x07F8
 MATH_START      EQU 0x07FC
 
 CMD_V4M4        EQU 1
@@ -26,7 +27,7 @@ oy      RN r4
 x       RN r5
 y       RN r6
 z       RN r7
-w       RN r8
+w       RN r8 ; unused
 
 px      RN z
 py      RN r9
@@ -41,8 +42,6 @@ input   RN lr
 proj_v4m4_super_asm
         stmfd sp!, {r4-r11, lr}
 
-        add last, dst, count, lsl #3
-
         mov	input, #(MADAM)
         add	input, input, #(MATH_STACK) ; MATRIX00
 
@@ -52,13 +51,12 @@ proj_v4m4_super_asm
         stmia input!, {r4-r11}          ; input = B0
 
         add output, input, #(8 * 4)     ; output = B1
-        mov cmd, #(CMD_V4M4)
 
-        mov w, #1 ; HACK TIPS: we can use cmd instead to save register! 8)
+        mov cmd, #(CMD_V4M4)
 
         ; start transform of the first vertex
         ldmia src!, {x, y, z}
-        stmia input, {x, y, z, w} ; (x, y, z, 1)
+        stmia input, {x, y, z} ; don't change 4th component (detault w = 1)
         str cmd, [input, #(MATH_START - MATH_STACK - 0x40)]
 
         ; pre-load division table pointer
@@ -67,11 +65,28 @@ proj_v4m4_super_asm
         mov ox, #CENTER_X
         mov oy, #CENTER_Y
 
+        add last, dst, count, lsl #3
+
+        ; fill 17 nop's to get enough time to calc the first vertex
+    GBLA cnt
+cnt SETA 0
+    WHILE cnt < 17
+cnt SETA cnt+1
+        nop
+    WEND
+
 loop
         ; read next vertex
         ldmia src!, {x, y, z}
+
+    IF {FALSE}
+        ; for debugging purposes, fail if the result isn't ready
+        ldr w, [input, #(MATH_STATUS - MATH_STACK - 0x40)]
+        tst w, #1
+        bne stop
+    ENDIF
         ; write the next vertex (MADAM had enough time)
-        stmia input, {x, y, z, w}
+        stmia input, {x, y, z}
         ; read transform result
         ldmia output, {x, y, z}
         ; start next vertex transform
@@ -88,6 +103,6 @@ loop
 
         cmp dst, last
         blt loop
-
+stop
         ldmfd sp!, {r4-r11, pc}
     END
